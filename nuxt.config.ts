fd058f1d955729a69c4d226a38e7e59a4a826045
robinwatson <drumsrobin@gmail.com>
2024-02-01 21:33:56 -0500

    add bare bones

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    '@nuxtjs/google-fonts',
+    // https://google-fonts.nuxtjs.org/getting-started/setup
+  ]
+})
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1 class="p-1 bg-purple-500 text-yellow-800">Index page</h1>
+		<NuxtLayout>
+			<NuxtPage/>
+		</NuxtLayout>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="postcss">
+/* https://ui.nuxt.com/components/carousel */
+</style>
