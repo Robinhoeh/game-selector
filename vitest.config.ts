a665b270421fe9076e0ee751a53d696d2e210d31
robinwatson <drumsrobin@gmail.com>
2024-02-05 21:01:39 -0500

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
 	test: {
 		environment: "nuxt",
-		include: ["**/*.{test, spec}.?(c|m)[jt]s?(x)"],
+		// include: ["**/*.{test, spec}.?(c|m)[jt]s?(x)"],
 		
 		//No need to import descibe, it, expect, etc...
 		globals: true
