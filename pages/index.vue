ee6f9fcc06057d616791fefee605d5cea682747f
robinwatson <drumsrobin@gmail.com>
2024-02-05 20:32:14 -0500

    add basic UI scaffold

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,87 @@
 	<NuxtLayout>
 		<h1 class="prose-1">Game Selector</h1>
 		<p>Vote on your favorite games to be chosen for team play at the end of the month</p>
+		<section class="py-3 mb-6">
+			<h3>Countdown to end of month</h3>
+			<div class="flex justify-around">
+				<div class="flex flex-col items-center">
+					<p>Days</p>
+					<p>00</p>
+				</div>
+				<div class="flex flex-col items-center">
+					<p>Hours</p>
+					<p>00</p>
+				</div>
+				<div class="flex flex-col items-center">
+					<p>Minutes</p>
+					<p>00</p>
+				</div>
+				<div class="flex flex-col items-center">
+					<p>Seconds</p>
+					<p>00</p>
+				</div>
+			</div>
+		</section>
+		<section class="py-3 mb-6">
+			<div class="flex justify-around">
+				<div>
+					<img src="" alt="">
+					<div>---- Game 1 ---</div>
+				</div>
+				<div>
+					<img src="" alt="">
+					<div>---- Game 2 ---</div>
+				</div>
+				<div>
+					<img src="" alt="">
+					<div>---- Game 3 ---</div>
+				</div>
+			</div>
+		</section>
+		<section class="py-3 mb-6">
+			<h3>Add your favorite games</h3>
+			<!-- form with 3 inputs, 1 for each card -->
+			<form>
+				<div class="flex justify-around">
+					<div>
+						<label for="game1">Game 1</label>
+						<input type="text" id="game1" name="game1">
+					</div>
+					<div>
+						<label for="game2">Game 2</label>
+						<input type="text" id="game2" name="game2">
+					</div>
+					<div>
+						<label for="game3">Game 3</label>
+						<input type="text" id="game3" name="game3">
+					</div>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</section>
+		<section class="py-3 mb-6">
+			<h3>Rankings</h3>
+			<div class="flex justify-around">
+				<!-- card component with list of games -->
+				<div>
+					<ul>
+						<li>game 1 </li>
+						<li>game 2</li>
+						<li>game 3</li>
+					</ul>
+				</div>
+				<div><ul>
+						<li>game 1 </li>
+						<li>game 2</li>
+						<li>game 3</li>
+					</ul></div>
+				<div><ul>
+						<li>game 1 </li>
+						<li>game 2</li>
+						<li>game 3</li>
+					</ul></div>
+			</div>
+		</section>
 	</NuxtLayout>
 </template>
 
