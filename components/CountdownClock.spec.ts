ee87383f6acb711096257eb6b3a4f1f1a47a939e
robinwatson <drumsrobin@gmail.com>
2024-02-12 21:03:39 -0500

    add live clock

--- components/CountdownClock.spec.ts
+++ components/CountdownClock.spec.ts
@@ -1,51 +1,68 @@
 import { describe, expect } from "vitest"
 import { format } from "date-fns"
 import { enCA } from "date-fns/locale"
-import { render, screen, fireEvent } from "@testing-library/vue"
-import  countdownClock from "./CountdownClock.vue"
+import { render, screen, fireEvent, rere } from "@testing-library/vue"
+import  CountdownClock from "./CountdownClock.vue"
 
 
 describe("CountdownClock", () => {
-	test("should display the countdown clock template", async() => {
-		render(countdownClock)
+	test("should display the countdown clock template", () => {
+		render(CountdownClock)
 		expect(screen.queryAllByTestId("countdown-clock")).toBeTruthy()
-		await fireEvent.copy(screen.getByText("Days"))
+		fireEvent.copy(screen.getByText("Days"))
+
 	});
 	
-	test("should contain a date object", async () => {
-		render(countdownClock)
+	test("should contain a date object", () => {
+		render(CountdownClock)
 		const date = new Date()
 		expect(date).toBeTruthy()
 	})
 
-	test("should display the current day and time", async () => {
-		render(countdownClock)
+	test("should display the current day and time", () => {
+		render(CountdownClock)
 		const date = new Date()
 		const formattedDate = format(date, 'dd, hh, :mm, :ss')
 		expect(formattedDate).toEqual(format(date, 'dd, hh, :mm, :ss'))
 	})
 	
-	test("should display the current time in Toronto", async () => {
-		render(countdownClock)
+	test("should display the current time in Toronto", () => {
+		render(CountdownClock)
 		const date = new Date()
 		const formattedDate = format(date, 'dd, hh, :mm, :ss', { locale: enCA })
 		expect(formattedDate).toEqual(format(date, 'dd, hh, :mm, :ss', { locale: enCA }))
 	})
 
-	test("Add the time and date stamps to each clock section", async () => {
-		render(countdownClock)
-		const remainingDays = format(new Date, 'dd')
-		const remainingHours = format(new Date, 'hh')
-		const remainingMinutes = format(new Date, ':mm')
-		const remainingSeconds = format(new Date, ':ss')
+	test("Add the time and date stamps to each clock section", () => {
+		render(CountdownClock)
+		let remainingDays = format(new Date, 'dd')
+		let remainingHours = format(new Date, 'hh')
+		let remainingMinutes = format(new Date, ':mm')
+		let remainingSeconds = format(new Date, ':ss')
 		expect(remainingDays).toContain(format(new Date, 'dd'))
 		expect(remainingHours).toContain(format(new Date, 'hh'))
 		expect(remainingMinutes).toContain(format(new Date, ':mm'))
 		expect(remainingSeconds).toContain(format(new Date, ':ss'))
 	})
+
+	test("Ensure clock runs in real time and displays", () => {
+		const time = () => {
+			let remainingDays = format(new Date, 'dd')
+			let remainingHours = format(new Date, 'hh')
+			let remainingMinutes = format(new Date, ':mm')
+			let remainingSeconds = format(new Date, ':ss')
+
+			let countdown = remainingDays + remainingHours + remainingMinutes + remainingSeconds
+			return countdown
+		}
+		setInterval(() => {
+			time()
+		}, 1000)
+	})
 })
 
 
+// 
 // Target countdown date is last day of each month
 // Countdown will reset to next month after reaching 0
 // Countdown will update every second
--- components/CountdownClock.vue
+++ components/CountdownClock.vue
@@ -5,8 +5,8 @@
 			<p>{{ remainingDays }}</p>
 		</div>
 		<div class="flex flex-col items-center">
+			<p>Remaining hours</p>
 			<p>{{ remainingHours}}</p>
-			<p>00</p>
 		</div>
 		<div class="flex flex-col items-center">
 			<p>Minutes</p>
@@ -23,11 +23,24 @@
 import { format } from 'date-fns'
 import { enCA } from 'date-fns/locale'
 const date = new Date()
-const formattedDate = format(date, 'dd, hh, :mm, :ss', { locale: enCA })
-const remainingDays = format(new Date, 'dd')
-const remainingHours = format(new Date, 'hh')
-const remainingMinutes = format(new Date, ':mm')
-const remainingSeconds = format(new Date, ':ss')
+let formattedDate = format(date, 'dd, hh, :mm, :ss', { locale: enCA })
+let remainingDays = ref(format(new Date, 'dd'))
+let remainingHours = ref(format(new Date, 'hh'))
+let remainingMinutes = ref(format(new Date, ':mm'))
+let remainingSeconds = ref(format(new Date, ':ss'))
+
+const displayTimeEverySecond = () => {
+	remainingDays.value = format(new Date, 'dd')
+	remainingHours.value = format(new Date, 'hh')
+	remainingMinutes.value = format(new Date, ':mm')
+	remainingSeconds.value = format(new Date, ':ss')
+}
+
+onBeforeMount(() => {
+	setInterval(() => {
+		displayTimeEverySecond()
+	}, 1000)
+})
 </script>
 
 <style scoped>
