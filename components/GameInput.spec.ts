79a1a99c0a6080546f9c001df3f1d298fd4b60a4
robinwatson <drumsrobin@gmail.com>
2024-02-15 19:53:21 -0500

    add form specs

---
+++ components/GameInput.spec.ts
@@ -0,0 +1,18 @@
+import { fireEvent, render, screen } from "@testing-library/vue";
+import GameInput from "./GameInput.vue";
+
+describe("GameInput", () => {
+
+})
+
+// form should render
+// form input should render
+// only one input needs to have a value to submit
+// when form submits, the input should be cleared
+// validation: check if input already exists in list(integration test)
+// should probably call an api to return a list of games to choose from
+// https://api-docs.igdb.com/#requests
+// should display a success message when game is added to list
+// should handle errors if submit fails
+// should handle error for rate limit 429
+
