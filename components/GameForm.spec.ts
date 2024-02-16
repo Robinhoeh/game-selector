29036605307fb679f72a1c0f3a6b525547c00670
robinwatson <drumsrobin@gmail.com>
2024-02-16 13:55:42 -0500

    add vue test utitls + mount

--- components/GameForm.spec.ts
+++ components/GameForm.spec.ts
@@ -1,20 +1,46 @@
 import { fireEvent, render, screen } from "@testing-library/vue";
 import { renderSuspended } from "@nuxt/test-utils/runtime";
+import { mount } from "@vue/test-utils"
 // import GameInput from "./GameInput.vue";
 import GameForm from "./GameForm.vue";
 import { plugin } from "@formkit/vue";
 import config from '../formkit.config'
 
+// describe("GameForm", () => {
+// 	test("GameForm should render", async () => {
+// 		mount(
+// 			{
+// 				template: '<GameForm />',
+// 			},
+// 			{
+// 				global: {
+// 					plugins: [[plugin, config]],
+// 				},
+// 			}
+// 		)
+// 		// const { debug } = render(GameForm)
+// 		// debug()
+// 		// expect(screen.getByTestId("game-form")).toBeDefined()
+// 		expect(GameForm).toBeTruthy()
+// 		// expect(screen.getAllByTestId("game-form")).toBeTruthy()
+// 		// expect(screen.queryAllByTestId("game-input")).toBeTruthy()
+// 	})
+// })
+
 describe("GameForm", () => {
 	test("GameForm should render", async () => {
-		await renderSuspended(GameForm, {
+		const wrapper = mount(GameForm, {
 			global: {
 				plugins: [[plugin, config]],
-			}
+			},
 		})
+			
+		
 		// const { debug } = render(GameForm)
 		// debug()
-		expect(screen.getByTestId("game-form")).toBeDefined()
+		// expect(screen.getByTestId("game-form")).toBeDefined()
+		expect(wrapper.find('[data-test-id="game-form"]').exists()).toBe(true)
+		// expect(screen.getAllByTestId("game-form")).toBeTruthy()
 		// expect(screen.queryAllByTestId("game-input")).toBeTruthy()
 	})
 })
