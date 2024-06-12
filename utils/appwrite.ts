0d1ab0f0a16b883019d9a881816e071f69c05d21
robinwatson <drumsrobin@gmail.com>
2024-06-11 22:18:44 -0400

    switch urls

--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -1,7 +1,7 @@
 import { Client, Account, OAuthProvider } from "appwrite";
 
 console.log(OAuthProvider)
-const url = import.meta.env.VITE_APPWRITE_ENDPOINT;
+const url = 'https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/google/660b4ac43ffdd541fbfe';
 const project = import.meta.env.VITE_APPWRITE_PROJECT;
 
 const client = new Client();
@@ -14,5 +14,5 @@ export const account = new Account(client);
 
 account.createOAuth2Session(
     OAuthProvider.Google,
-    'https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/google/660b4ac43ffdd541fbfe'
+    'videogame-selector.netlify.app"'
 );
