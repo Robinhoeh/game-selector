9ea4056cfb0c94d615c7cc228482121074b89ef8
robinwatson <drumsrobin@gmail.com>
2024-06-11 22:21:06 -0400

    add 3rd param

--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -1,7 +1,7 @@
 import { Client, Account, OAuthProvider } from "appwrite";
 
 console.log(OAuthProvider)
-const url = 'https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/google/660b4ac43ffdd541fbfe';
+const url = import.meta.env.VITE_APPWRITE_ENDPOINT;
 const project = import.meta.env.VITE_APPWRITE_PROJECT;
 
 const client = new Client();
@@ -14,5 +14,6 @@ export const account = new Account(client);
 
 account.createOAuth2Session(
     OAuthProvider.Google,
-    'videogame-selector.netlify.app"'
+	'https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/google/660b4ac43ffdd541fbfe',
+	'https://cloud.appwrite.io/v1/account/sessions/oauth2/callback/google/660b4ac43ffdd541fbfe'
 );
