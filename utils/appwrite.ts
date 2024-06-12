964c8ddedd68445be7f43f57a808ca7810be053d
robinwatson <drumsrobin@gmail.com>
2024-06-11 21:38:52 -0400

    lets try google auth again

--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -1,34 +1,20 @@
-// import { Client, Account, OAuthProvider } from "appwrite";
+import { Client, Account, OAuthProvider } from "appwrite";
 
-// console.log(OAuthProvider)
-// const url = import.meta.env.VITE_APPWRITE_ENDPOINT;
-// const project = import.meta.env.VITE_APPWRITE_PROJECT;
+console.log(OAuthProvider)
+const url = import.meta.env.VITE_APPWRITE_ENDPOINT;
+const project = import.meta.env.VITE_APPWRITE_PROJECT;
 
-// const client = new Client();
-
-// client
-//     .setEndpoint(url)
-//     .setProject(project);
-
-// export const account = new Account(client);
-
-// account.createOAuth2Session(
-//     OAuthProvider.Google,
-//     'videogame-selector.netlify.app',
-//     'videogame-selector.netlify.app/',
-//     ['https://www.googleapis.com/auth/userinfo.email']
-// );
-
-
-import { Client, Databases, Account } from "appwrite"
-
-const url: string = import.meta.env.VITE_APPWRITE_ENDPOINT
-const project: string = import.meta.env.VITE_APPWRITE_PROJECT
-
-const client: Client = new Client()
+const client = new Client();
 
 client
-	.setEndpoint(url).setProject(project)
+    .setEndpoint(url)
+    .setProject(project);
 
-export const account: Account = new Account(client)
-export const database: Databases = new Databases(client)
+export const account = new Account(client);
+
+account.createOAuth2Session(
+    OAuthProvider.Google,
+    'videogame-selector.netlify.app',
+    'videogame-selector.netlify.app/',
+    ['https://www.googleapis.com/auth/userinfo.email']
+);
